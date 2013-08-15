72c0af20b1ec880d95b3953bc8261a0e8652874c
Andrew Mains <andrew.mains@kontagent.com>
2013-08-15 10:12:40 -0700

    Upgraded to clojure 1.5

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject resume-builder "0.1.0-SNAPSHOT"
             :description "A simple resume builder"
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.5.0"]
                            [noir "1.3.0-beta2"]
                            [hiccup "1.0.0"]
                            [fs "1.3.2"]
